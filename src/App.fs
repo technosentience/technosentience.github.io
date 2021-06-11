eb2f5763c9e318c2a2a112bc07e69766b8d033aa
OperatorGK <operator.gk@gmail.com>
2021-06-11 19:47:33 +0300

    Revert "temp hosting v2"
    This reverts commit da4f46a68c7560288c6890b76ea76af583327005.

---
+++ src/App.fs
@@ -0,0 +1,13 @@
+module App
+
+open View
+open Update
+open Init
+open Input
+
+open Elmish
+
+Program.mkSimple init update view
+|> Program.withSubscription mouse
+|> Program.withSubscription timer
+|> Program.run
